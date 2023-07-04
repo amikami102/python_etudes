7085453e23f185638bf8361d5c960d6b2304c97d
amikami102 <asakomikami@gmail.com>
2023-07-04 16:45:19 -0400

    revisited exercise, solved all bonuses

---
+++ python_morsels/compact.py
@@ -0,0 +1,35 @@
+# compact.py
+"""
+A script defining `compact()`, which accepts a sequence and returns a new iterable with adjacent duplicate values removed.
+"""
+from typing import *
+
+SENTINEL = object()
+
+
+def compact(iterable: Iterable) -> Iterator:
+    """ Remove adjacent duplicate values in `sequence`. """
+    it = iter(iterable)
+    previous = next(it, SENTINEL)
+    if previous is SENTINEL:
+        return ()
+    
+    yield previous
+    for item in it:
+        if previous != item:
+            yield item
+            previous = item
+            
+
+# base problem
+assert list(compact([1, 1, 1])) == [1]
+assert list(compact([1, 1, 2, 2, 3, 2])) == [1, 2, 3, 2]
+assert list(compact([])) == []
+assert list(compact([None, 0, "", []])) == [None, 0, "", []]
+
+# bonus 1, test with any iterable
+assert list(compact(n**2 for n in [1, 2, 2])) == [1, 4]
+
+# bonus 2, test that `compac()` returns an iterator
+c = compact(n**2 for n in [1, 2, 2])
+assert iter(c) is c
