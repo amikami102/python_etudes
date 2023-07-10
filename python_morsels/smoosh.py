2f09fb1bce5cb7d429b3ba0c4fc240354b4c9362
amikami102 <asakomikami@gmail.com>
2023-07-10 17:49:43 -0400

    solved bonus 2

--- python_morsels/smoosh.py
+++ python_morsels/smoosh.py
@@ -3,11 +3,13 @@
 A script defining utilities that flatten an iterable of iterables `n` levels deep.
 """
 from typing import *
+import re
 from functools import partial
 
 T = TypeVar('T')
+SMOOSH_RE = re.compile(r'^smoo+sh$')
 
-def smoosh(nested_iterable: Iterable[Iterable[T]], n: int = 1) -> Iterator[T]:
+def _smoosh(nested_iterable: Iterable[Iterable[T]], n: int = 1) -> Iterator[T]:
     """ Generate a flattened `nested_iterable` `n` level deep. """
     for sublist in nested_iterable:
         if isinstance(sublist, Iterable) and not isinstance(sublist, str) and n:
@@ -15,12 +17,22 @@ def smoosh(nested_iterable: Iterable[Iterable[T]], n: int = 1) -> Iterator[T]:
         else:
             yield sublist
 
+smoosh = _smoosh
+smooosh = partial(_smoosh, n=2)
+smoooosh = partial(_smoosh, n=3)
+smooooosh = partial(_smoosh, n=4)
+smoooooosh = partial(_smoosh, n=5)
+smooooooosh = partial(_smoosh, n=6)
 
-smooosh = partial(smoosh, n=2)
-smoooosh = partial(smoosh, n=3)
-smooooosh = partial(smoosh, n=4)
-smoooooosh = partial(smoosh, n=5)
-smooooooosh = partial(smoosh, n=6)
+
+def __getattr__(name: str) -> Callable:
+    """ Add any variant of `_smoosh` to the module by making a dynamic attribute. """
+    if SMOOSH_re.match(name):
+        levels = len(name.lstrip('smo').rstrip('sh'))
+        return partial(_smoosh, n=levels)
+    else:
+        raise AttributeError(f"module {__name__} has no attribute {name}")
+    
 
 # base problem, test `smoosh()`
 matrix = [[1, 2, 3], [4, 5, 6]]
@@ -67,3 +79,5 @@ assert list(smoooosh(data)) == [2, 1, 3, [4, 7, [11, [18, 29], 40, 69], 109], 17
 assert list(smooooosh(data)) == [2, 1, 3, 4, 7, [11, [18, 29], 40, 69], 109, 178]
 assert list(smoooooosh(data)) == [2, 1, 3, 4, 7, 11, [18, 29], 40, 69, 109, 178]
 assert list(smooooooosh(data)) == [2, 1, 3, 4, 7, 11, 18, 29, 40, 69, 109, 178]
+
+# bonus 2, make the module `smoosh` support any variant of `smoosh` function
