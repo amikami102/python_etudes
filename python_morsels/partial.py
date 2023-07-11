03a26e50dc80c489e2c86277bbc20feb1171f181
amikami102 <asakomikami@gmail.com>
2023-07-11 14:19:16 -0400

    solved  up to bonus 3

---
+++ python_morsels/partial.py
@@ -0,0 +1,84 @@
+# partial.py
+"""
+A script defining `partial` callable that allows for partial evaluation of given functions.
+"""
+from typing import *
+
+SKIP = object()
+
+
+def fill_in_skipped_args(placeholders: tuple, fillers: tuple) -> tuple:
+    """ Fill in skpped arguments in `placeholders` with `fillers`. """
+    if not fillers:
+        return placeholders
+    fillers = iter(fillers)
+    filled = (
+        arg if arg is not SKIP else next(fillers)
+        for arg in placeholders
+    )
+    return (*filled, *fillers)
+
+
+class partial:
+    """ A class that impelments partial evaluation of a given function. """
+    def __init__(self, func, *args, **kwargs):
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+    
+    def __call__(self, *args, **kwargs):
+        all_args = tuple(fill_in_skipped_args(self.args, args))
+        if SKIP in all_args:
+            raise TypeError('Not enough positional argumnets')
+        all_kwargs = {**self.kwargs, **kwargs}
+        return self.func(*all_args, **all_kwargs)
+    
+    def partial(self, *args, **kwargs) -> 'partial':
+        return partial(self, *args, **kwargs)
+
+
+# base problem can be solved by `from functools import partial`
+print_lines = partial(print, sep='', end='')
+print_lines('a\n', 'b\n', 'c\n')
+enumerate1 = partial(enumerate, start=1)
+assert list(enumerate1(['a', 'b', 'c'])) == [(1, 'a'), (2, 'b'), (3, 'c')]
+all_to_strings = partial(map, str)
+assert list(all_to_strings([1, 2, 3])) == ['1', '2', '3']
+
+# bonus 1, test that `partial()` can skip positional arguments
+has_length = partial(hasattr, SKIP, '__len__')
+assert not has_length(4)
+assert has_length([])
+has_length = partial(hasattr, SKIP, '__len__')
+try:
+    has_length()
+except TypeError:
+    print('Passed')
+else:
+    print('Failed')
+template = "{0} {1} {2} and {3}"
+skip_last = partial(template.format, 1, 2, SKIP)
+try:
+    skip_last(3)
+except Exception:
+    print('Passed')
+else:
+    print('Failed')
+    
+# bonus 2, test that `partial()` returns an object with `partial` method
+print_a_thing = partial(print, 1)
+print_two_things = print_a_thing.partial(2)
+print_more_things = print_two_things.partial(3, 4)
+print_more_things() #1 2 3 4
+print_more_things(sep=', ') #1, 2, 3, 4
+
+# make `partial` cllable work as a decorator
+@partial
+def multiply(numbers):
+    product = 1
+    for n in numbers:
+        product *= n
+    return product
+
+print_more_things_with_comma = print_more_things.partial(sep=', ')
+print_more_things_with_comma.partial(sep=' ')() #1 2 3 4
