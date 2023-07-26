d2c144e3abd5a367d4e655e65383d9db93e35205
amikami102 <asakomikami@gmail.com>
2023-07-26 16:19:13 -0400

    add files

---
+++ automate_boring_stuff/sandwich_maker.py
@@ -0,0 +1,9 @@
+# sandwich_maker.py
+"""
+A program that asks users for their sandwich preferences using `PyInputPlus` module.
+Sandwich ingredients and their marginal prices are stored in "sandwich.json" file.
+"""
+import json
+import pyinputplus as pyip
+
+
