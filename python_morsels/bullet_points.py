f20607e73c7d3d8c9399a0f182fc15a739172ce8
amikami102 <asakomikami@gmail.com>
2023-09-18 19:10:53 -0400

    solved bonus 2

--- python_morsels/bullet_points.py
+++ python_morsels/bullet_points.py
@@ -3,49 +3,78 @@
 A program that parses a bullet list.
 """
 from typing import *
-from collections import deque
+from collections import UserList
 from dataclasses import dataclass, field
+from textwrap import indent, dedent
 
 BULLET = '- '
 INDENT_SIZE = 4
 
+
+class BulletList(UserList):
+    """A sequence of bullets."""
+    def __str__(self) -> str:
+        return '\n'.join([str(bullet) for bullet in self.data])
+    
+    def filter(self, pattern: str = '') -> 'BulletList':
+        """
+        Return bullets that contain `pattern` in their texts or children's.
+        """
+        filtered = BulletList()
+        for bullet in self.data:
+            continue
+            # TODO: think of adding a new bullet with different set of children
+        return filtered
+
 @dataclass
 class Bullet:
     text: str
-    children: list['Bullet'] = field(init=False, default_factory=list)
+    children: BulletList = field(init=False, default_factory=BulletList)
+    parent: 'Bullet' = field(default=None)
     
     @classmethod
-    def create_bullet(cls, bullet_line: str) -> tuple[int, 'Bullet']:
-        """ Return a tuple of the level of `bullet_line` and `Bullet` object. """
-        levels, _, text = bullet_line.partition(BULLET)
-        levels: int = len(levels) // INDENT_SIZE
-        return levels, Bullet(text)
+    def create_bullet(cls, line: str) -> tuple[int, 'Bullet']:
+        """Return a tuple of the level of `line` and `Bullet` object."""
+        n_tabs, _, text = line.partition(BULLET)
+        return len(n_tabs) // INDENT_SIZE, Bullet(text)
     
     def add_child(self, child: 'Bullet') -> None:
+        child.parent = self
         self.children.append(child)
-        
     
     def __str__(self) -> str:
-        return f'{BULLET}{self.text}'
+        return '\n'.join(print_bullets(self))
 
 
 def parse_bullets(text: str) -> list[Bullet]:
-    """ Returns a list of `Bullet` objects parsed from `text`. """
-    bullets: list[Bullet] = []
+    """Returns a list of `Bullet` objects parsed from `text`."""
+    bullets: BulletList = BulletList()
     parents: dict[int, Bullet] = {}
     
     for line in text.splitlines():
         levels, bullet = Bullet.create_bullet(line)
-        if not levels:
+        if levels == 0:
             bullets.append(bullet)
             parents.clear() # clear the dictionary to build a new tree
-            parents.update({0: bullet})
         else:
             parents[levels - 1].add_child(bullet)
-        
         parents.update({levels: bullet})
     return bullets
 
+
+def print_bullets(bullet:Bullet, level: int = 0) -> list[str]:
+    """Return string representation of `bullet` including its children."""
+    #TODO: simplify this code. Indenting an indented line
+    # is equivalent to indenting by two tabs.
+    tab: str = ' ' * INDENT_SIZE
+    my_bullets = [indent(f'{BULLET}{bullet.text}', tab * level)]
+    for child in bullet.children:
+        if child.children:
+            my_bullets.extend(print_bullets(child, level+1))
+        else:
+            my_bullets.append(indent(f'{BULLET}{child.text}', tab * (level+1)))
+    return my_bullets
+    
 
 # base problem, test `parse_bullets()`
 data = """\
@@ -72,3 +101,66 @@ bullets = parse_bullets(data)
 assert not len(bullets[0].children)
 assert len(bullets[1].children) == 2
 assert bullets[1].children[0].children[1].text == 'Write tests'
+
+# bonus 2, test `parent` attribute
+data = """
+- Chapter 1
+     - Section A
+         - Sub-section i
+     - Section B
+""".strip()
+my_bullets = parse_bullets(data)
+chapter1 = my_bullets[0]
+section_a = chapter1.children[0]
+subsection1 = section_a.children[0]
+assert subsection1.parent == section_a
+assert section_a.parent == chapter1
+assert chapter1.parent == None
+assert str(my_bullets[0]) == dedent("""
+- Chapter 1
+    - Section A
+        - Sub-section i
+    - Section B
+""").strip('\n')
+assert str(my_bullets[0].children[0]) == dedent("""
+- Section A
+    - Sub-section i
+""").strip('\n')
+assert str(my_bullets[0].children[1]) == dedent("""
+- Section B
+""").strip('\n')
+
+# bonus 3, test `parse_bullet()` output's string representation 
+data = """
+- Buy groceries
+- Python Morsels
+     - Fix bug on exercise submission page
+         - Figure out how to reproduce bug
+         - Write tests
+         - Write and deploy fix
+     - Write a new exercise
+""".strip()
+my_bullets = parse_bullets(data)
+assert str(my_bullets) == """
+- Buy groceries
+- Python Morsels
+    - Fix bug on exercise submission page
+        - Figure out how to reproduce bug
+        - Write tests
+        - Write and deploy fix
+    - Write a new exercise
+""".strip()
+assert str(my_bullets.filter("fix")) == dedent("""
+- Python Morsels
+    - Fix bug on exercise submission page
+        - Write and deploy fix
+""").strip()
+results = my_bullets.filter("write")
+assert str(results) == dedent("""
+- Python Morsels
+    - Fix bug on exercise submission page
+        - Write tests
+        - Write and deploy fix
+    - Write a new exercise
+""").strip()
+assert results[0].children[0].text == 'Fix bug on exercise submission page'
