b0fcf58872d74e4d5913fa20dbe6ea3793186ed7
amikami102 <asakomikami@gmail.com>
2023-09-21 15:06:23 -0400

    solved bonus 2

--- python_morsels/reprtools.py
+++ python_morsels/reprtools.py
@@ -6,29 +6,39 @@ method for Python classes.
 from typing import *
 from operator import attrgetter
 
-from rich import print
-
 T = TypeVar('T')
 
 
 def format_arguments(*args, **kwargs) -> str:
-    """ Return a programmer-readable string representation of positional and keyword arguments. """
-    arguments: tuple[str, ...] = *(repr(arg) for arg in args),\
-        *(f'{key}={value!r}' for key, value in kwargs.items())
-    return ', '.join(arguments)
+    """
+    Return a programmer-readable string representation
+    of `*args` and `**kwargs`.
+    """
+    arg_strings  = (repr(arg) for arg in args)
+    kwarg_strings = (f'{key}={value!r}' for key, value in kwargs.items())
+    return ', '.join([*arg_strings, *kwarg_strings])
 
 
-def make_repr(args: list[str] = None, kwargs: list[str] = None) -> Callable:
-    """ Accept `*args` and `**kwargs` attributes and return an appropriate `__repr__` method for the class it's attached to. """
+def make_repr(*, args: list[str] = (), kwargs: list[str] = ()) -> Callable:
+    """
+    Return a `__repr__` method that formats attributes named in `args`
+    and `kwargs` with `format_arguments()`.
+    """
+    def repr_method(self: T) -> str:
+        """A ` __repr__` method."""
+        arg_attrib = (getattr(self, arg) for arg in args)
+        kwarg_attrib = {kwarg: getattr(self, kwarg) for kwarg in kwargs}
+        return f'{type(self).__name__}({format_arguments(*arg_attrib, **kwarg_attrib)})'
     
-    def func(obj: T) -> str:
-        arg_attrib = (getattr(obj, arg) for arg in args) if args else ()
-        kwarg_attrib = {kwarg: getattr(obj, kwarg) for kwarg in kwargs} if kwargs else {}
-        
-        class_attr = format_arguments(*arg_attrib, **kwarg_attrib)
-        return f'{type(obj).__name__}({class_attr})'
-    
-    return func
+    return repr_method
+
+
+def auto_repr(*, args: list[str] = (), kwargs: list[str] = ()) -> Callable:
+    """Return a decorator that adds `__repr__` method to the class passed into it."""
+    def decorator(cls):
+        cls.__repr__ = make_repr(args=args, kwargs=kwargs)
+        return cls
+    return decorator
 
 
 # base problem, test `format_arguments()`
@@ -44,13 +54,20 @@ class Point:
     
     __repr__ = make_repr(args=['x', 'y'], kwargs=['color'])
 
-
 class Empty:
     __repr__ = make_repr()
-
 
 assert repr(Point(1, 2)) == "Point(1, 2, color='purple')"
 assert repr(Point(x=3, y=4, color='green')) == "Point(3, 4, color='green')"
 assert str(Empty()) == "Empty()"
 assert repr(Empty()) == "Empty()"
-            
+
+# bonus 2, test `@auto_repr()` decorator
+@auto_repr(args=['x', 'y'], kwargs=['color'])
+class Point:
+    def __init__(self, x, y, color='purple'):
+        self.x, self.y = x, y
+        self.color = color
+
+assert repr(Point(1, 2)) == "Point(1, 2, color='purple')"
+assert repr(Point(x=3, y=4, color='green')) == "Point(3, 4, color='green')"
