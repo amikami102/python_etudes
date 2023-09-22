b508bdd10986394cf40c9408dc5f30fa0c01e88c
amikami102 <asakomikami@gmail.com>
2023-09-22 14:45:09 -0400

    Solved up to bonus 3

---
+++ python_morsels/alias.py
@@ -0,0 +1,55 @@
+# alias.py
+"""
+A script defining `alias`, a descriptor that makes aliases for class attributes.
+"""
+from typing import *
+
+class alias:
+    def __init__(self, attribute: str, *, write: bool = False):
+        self.attribute = attribute
+        self.write = write
+        
+    def __get__(self, obj, objtype=None) -> Any:
+        return getattr(obj, self.attribute)
+    
+    def __set__(self, obj, value) -> None:
+        if not self.write:
+            raise AttributeError
+        setattr(obj, self.attribute, value)
+
+
+# base problem
+class DataRecord:
+    title = alias('serial')
+    def __init__(self, serial):
+        self.serial = serial
+
+record = DataRecord("148P")
+assert record.title == '148P'
+assert record.serial == '148P'
+record.serial = '148X'
+assert record.title == '148X'
+
+# bonus 1, test alias assignment
+class DataRecord:
+    title = alias('serial')
+    def __init__(self, serial):
+        self.serial = serial
+
+record = DataRecord("148X")
+try:
+    record.title = "149S"
+except AttributeError:
+    print('passed')
+else:
+    print('failed')
+
+# bonus 2, accept `write` keyword argument
+class DataRecord:
+    title = alias('serial', write=True)
+    def __init__(self, serial):
+        self.serial = serial
+
+record = DataRecord("192B")
+record.title = "192A"
+assert record.serial == '192A'
