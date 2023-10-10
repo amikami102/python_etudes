8fbd465ed90891f3e8da8f3ea255a04b18e4ceaf
amikami102 <asakomikami@gmail.com>
2023-10-10 16:04:32 -0400

    solved bonus 2

--- python_morsels/iconv.py
+++ python_morsels/iconv.py
@@ -1,6 +1,6 @@
 # iconv.py
 """
-A program that converts a text file's character encoding like Linux CLI tool's `iconv`.
+A program that converts a text file's character encoding.
 
 Usage example:
     $ python iconv.py utf8_file.txt -f utf-8 -o utf16le_file.txt -t utf-16le
@@ -10,14 +10,19 @@ Usage example:
 import argparse
 import sys
 
+
 parser = argparse.ArgumentParser("Convert a file's character encoding")
-parser.add_argument('input', type=str, nargs='?')
-parser.add_argument('--from-code', '-f', type=str, default='utf-8')
-parser.add_argument('--to-code', '-t', type=str, default='utf-8')
-parser.add_argument('--output', '-o', type=argparse.FileType('w'), default='-')
+parser.add_argument('file', help='input file',
+                    type=argparse.FileType('r'), default='-', nargs='?')
+parser.add_argument('--from-code', '-f', help='encoding of original file')
+parser.add_argument('--to-code', '-t', help='encoding of output')
+parser.add_argument('--output', '-o', help='output file',
+                    type=argparse.FileType('w'), default='-')
 args = parser.parse_args()
 
 
-data = open(args.input, mode='rb').read().decode(args.from_code)
+args.file.reconfigure(encoding=args.from_code)
 args.output.reconfigure(encoding=args.to_code)
-args.output.write(data)
+
+for line in args.file:
+        args.output.write(line)    
