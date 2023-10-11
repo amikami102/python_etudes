d4fcbeed0338958db67aca481cc49bcba87dc378
amikami102 <asakomikami@gmail.com>
2023-10-10 22:26:18 -0400

    solved up to bonus 3

--- python_morsels/loop_helper.py
+++ python_morsels/loop_helper.py
@@ -2,38 +2,54 @@
 """
 A script defining `loop_helper()` function, which gives metadata while iterating.
 """
-
-from rich import print
-from typing import *
-import collections
-import itertools
+from typing import Iterator, Iterable, TypeVar
+from dataclasses import dataclass
+from itertools import pairwise
 
 T = TypeVar('T')
-LoopInfo = collections.namedtuple('LoopInfo', ['is_first', 'index', 'current', 'previous'])
+SENTINEL = object()
+
+@dataclass
+class LoopInfo:
+    is_first: bool
+    index: int
+    current: T
+    previous: T
+    next: T
+    is_last: bool
 
 
-def loop_helper(iterable: Iterable[T], previous_default: T = None) -> Iterator[tuple[T, LoopInfo]]:
-    history = (previous_default, *iterable)
-    return (
-        (
-            item,
-            LoopInfo(i == 0, i, item, prev)
-        )
-        for (i, item), prev in zip(enumerate(iterable), history)
-    )
-
+def loop_helper(
+        iterable: Iterable[T],
+        previous_default: T = None, next_default: T = None
+    ) -> Iterator[tuple[T, LoopInfo]]:
+    """
+    Iteratively yield a tuple of an item from the iterable and LoopInfo object.
+    """
+    previous = previous_default
+    it, it2 = tee(iterable)
+    for i, current in enumerate(it):
+        next_item = next(it2, SENTINEL)
+        yield current, LoopInfo(
+            i==0,
+            i, current, previous, next_item, 
+            next_item is SENTINEL
+            )
+        previous = current
+        
     
-
+# base problem
 colors = ["red", "blue", "green"]
-for color, info in loop_helper(colors):
-    if info.is_first:
-        print("This is the first color!")
-    print(f"Color {info.index} is {color}")
+colors_helper = loop_helper(colors)
+_, info = next(colors_helper)
+assert info.is_first
+assert info.index == 0
+assert not next(colors_helper)[1].is_first
+assert list(loop_helper([])) == []
 
-print(loop_helper([]))
+# bonus 1, test that `loop_helper` returns an iterator
+colors = ["red", "blue", "green"]
+colors_helper = loop_helper(colors)
+assert iter(colors_helper) == colors_helper
 
-for color, info in loop_helper(colors):
-    if info.is_first:
-        print(f"{color.title()} is the best color!")
-    else:
-        print(f"No {info.current} is the best color, not {info.previous}!")
+# bonus 2, test `previous` and `current` attributes
