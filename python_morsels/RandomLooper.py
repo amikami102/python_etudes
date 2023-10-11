4def229ad4d0142187f05d9379294ea8180c6b7d
amikami102 <asakomikami@gmail.com>
2023-10-10 22:40:12 -0400

    solved all bonuses

--- python_morsels/RandomLooper.py
+++ python_morsels/RandomLooper.py
@@ -1,17 +1,5 @@
 # RandomLooper.py
-"""
-A script for RandomLooper class object. 
-
->>> colors = ["red", "blue", "green", "purple"]
->>> for color in RandomLooper(colors):
-...     print(color)
-...
-green
-red
-purple
-blue
-"""
-from rich import print
+"""A script for RandomLooper class object."""
 from typing import *
 import random
 import itertools
@@ -21,16 +9,18 @@ T = TypeVar('T')
 class RandomLooper:
     
     def __init__(self, *iterables):
-        self._iterable = list(itertools.chain(*iterables))	# makes a shallow copy
+        self.items = [
+            item
+            for iterable in iterables
+            for item in iterable
+        ]
     
     def __iter__(self) -> Iterator[T]:
-        random.shuffle(self._iterable)
-        yield from self._iterable
+        random.shuffle(self.items)
+        yield from self.items
     
     def __len__(self) -> int:
-        return len(self._iterable)
-    
-    
+        return len(self.items)
 
 
 colors = ["red", "blue", "green", "purple"]
@@ -40,9 +30,8 @@ for color in RandomLooper(colors):
 # Return the original iterable unshuffled
 numbers = [1, 2, 3, 4]
 looper = RandomLooper(numbers)
-list(looper)
 print(numbers)
-print(looper._iterable)
+print(looper.items)
 
 shapes = ["square", "circle", "triangle", "octagon"]
 print(len(RandomLooper(colors, shapes)))
