8889108f773bb99326c933fabc044f145290f9b5
amikami102 <asakomikami@gmail.com>
2023-10-15 17:00:28 -0400

    solved all bonuses

--- python_morsels/SequenceZip.py
+++ python_morsels/SequenceZip.py
@@ -1,85 +1,54 @@
 # SequenceZip.py
-"""
-A script implementing `SequenceZip` class object.
-"""
-from rich import print
+"""A script implementing `SequenceZip` class object."""
 from typing import *
-import itertools
 
 
-class SequenceZip(Sequence):
-    """A special-purposed class that acts like built-in `zip` class."""
-    
-    def __init__(self, *iterables):
-        self.iterables = iterables
-    
-    def __getitem__(self, index) -> Sequence:
-        return tuple(iterable[index] for iterable in self.iterables)
-    
+class SequenceZip:
+    """A class that acts like `zip` for sequences."""
+
+    def __init__(self, *sequences):
+        self.sequences = sequences
+
     def __len__(self) -> int:
-        """
-        Lenth of SequenceZip is defined as the shortest of the sequences,
-        not the number of sequences.
-        """
-        return min(
-            (len(s) for s in self.iterables),
-            default=0
-        )
-    
+        """Return the length of the shortest sequence."""
+        return min((len(seq) for seq in self.sequences), default=0)
+
+    def __getitem__(self, index: int|slice) -> Sequence:
+        if isinstance(index, int):
+            return tuple(seq[:len(self)][index] for seq in self.sequences)
+        else:
+            indices = range(*index.indices(len(self)))
+            return SequenceZip(
+                *([seq[i] for i in indices] for seq in self.sequences)
+            )
+
     def __repr__(self) -> str:
-        body = ', '.join(repr(iterable) for iterable in self.iterables)
-        return f'{type(self).__name__}' + '(' + body + ')'
-    
+        sequences_str = ', '.join(repr(seq) for seq in self.sequences)
+        return f'{type(self).__name__}({sequences_str})'
+
     def __eq__(self, other: 'SequenceZip') -> bool:
-        """
-        Since `self` and `other` could have different number of sequences,
-        only compare as many as the shorter of the two.
-        e.g. `SequenceZip('hiya', [1, 2, 3]) has length 3,
-        so it will only be compared up to ('y', 3).
-        """
-        if not isinstance(other, SequenceZip):
+        if isinstance(other, SequenceZip):
+            return len(self) == len(other) and\
+                all(seq1 == seq2 for seq1, seq2 in zip(self, other))
+        else:
             return NotImplemented
-        
-        my_seq = tuple(
-            tuple(s[:len(self)]) for s in self.iterables
-        )
-        other_seq = tuple(
-            tuple(s[:len(other)]) for s in other.iterables
-        )
-        return tuple(my_seq) == tuple(other_seq)
-    
+
 
 # base problem
 zipped = SequenceZip('ABC', ['as', 'easy', 'as'], [1, 2, 3])
-for x, y, z in zipped:
-    print(x, y, z)
-print(list(zipped))
-print(len(zipped))
-print(zipped[1])
-x, y, z = [1, 2, 3], [4, 5, 6], [7, 8, 9]
-seq1 = SequenceZip(x, y, z)
-print(seq1[-1])
-assert seq1[-1] == (3, 6, 9)
-x[-1], z[-1] = z[-1], x[-1]
-print(seq1[-1])
-assert seq1[-1] == (9, 6, 3)
+assert list(zipped) == [('A', 'as', 1), ('B', 'easy', 2), ('C', 'as', 3)]
+assert len(zipped) == 3
+assert zipped[1] == ('B', 'easy', 2)
 
-# bonus 1
-print(zipped)
+# bonus 1, test string representation
+zipped = SequenceZip('ABC', ['as', 'easy', 'as'], [1, 2, 3])
+assert repr(zipped) == "SequenceZip('ABC', ['as', 'easy', 'as'], [1, 2, 3])"
+assert repr(SequenceZip(range(10), "hello"))== "SequenceZip(range(0, 10), 'hello')"
 
-# bonus 2
+# bonus 2, test equality
 assert SequenceZip('hiya', [1, 2]) == SequenceZip('hi', [1, 2, 3])
-print(list(SequenceZip('hiya', [1, 2, 3])))
-print(list(SequenceZip('hi', [1, 2, 3])))
-my_iterables = 'hiya', [1, 2, 3]
-other_iterables = 'hi', [1, 2, 3]
-print(
-    tuple(
-       tuple(s[:len(my_iterables)]) for s in my_iterables
-    )
-)
-print(
-    tuple(
-       tuple(s[:len(other_iterables)]) for s in other_iterables
-    )
-)
+assert SequenceZip('hiya', [1, 2, 3]) != SequenceZip('hi', [1, 2, 3])
+
+# bonus 3, test slicing
+zipped = SequenceZip('ABCDE', [1, 2, 3, 4, 5])
+assert zipped[1:] == SequenceZip('BCDE', [2, 3, 4, 5])
