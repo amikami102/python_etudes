15e3d69ff866e90b6c5c7b6f70acbb175006a5fc
amikami102 <asakomikami@gmail.com>
2023-10-28 00:01:49 -0400

    solved up to bonus 3

--- python_morsels/markdownify.py
+++ python_morsels/markdownify.py
@@ -2,28 +2,21 @@
 """
 A script implementing `markdownify()` function that converts HTML to markdown.
 """
-from rich import print
 import re
 
+SPACES_RE = re.compile(r'\s+')
+PARAGRAPH_RE = re.compile(r'<p>(.+?)</p>', re.DOTALL)
+LINEBREAK_RE = re.compile(r'<br>')
+STRONG_RE = re.compile(r"<strong>(.*?)</strong>", re.DOTALL)
 
-SPACES_RE = re.compile(r'\s+')							# two ore more white spaces
-PARAGRAPHS_RE = re.compile(r'<p>(.+?)</p>')				# <p>...</p> tag
-LINEBREAKS_RE = re.compile(r'<br>')						# <br> tag
 
-
-def markdownify(html_text: str) -> str:
-    """
-    Format `html_text` written in html to markdown by doing the following:
-    
-        - Handles whitespace normalization where consecutive linebreaks or spaces
-        are collapsed into single space character.
-        - Translates `<p>` tags so that paragraphs have two line breaks between them.
-        - Convert `<br>` tags to two white spaces followed by a newline character.
-    """
-    html_text = SPACES_RE.sub(r' ', html_text)
-    html_text = PARAGRAPHS_RE.sub(r'\1\n\n', html_text) # matches group(1) of the regex match
-    html_text = LINEBREAKS_RE.sub(r'  \n', html_text)
-    return html_text.strip()
+def markdownify(text: str) -> str:
+    """Normalize whitespaces and trandslate paragraphs."""
+    text = SPACES_RE.sub(r' ', text)
+    text = PARAGRAPH_RE.sub(r'\1\n\n', text)
+    text = LINEBREAK_RE.sub(r'  \n', text)
+    text = STRONG_RE.sub(r'**\1**', text)
+    return text.strip() 
 
 
 # base problem
@@ -39,7 +32,7 @@ html = "This text\nhas words over\nmultiple lines"
 text = "This text has words over multiple lines"
 assert markdownify(html) == text
 
-# bonus 1
+# bonus 1, test linebreak
 html = "Text with<br>linebreaks!"
 text = "Text with  \nlinebreaks!"
 assert markdownify(html) == text
@@ -47,3 +40,7 @@ assert markdownify(html) == text
 html = "<p>A paragraph with a<br>linebreak</p>"
 text = "A paragraph with a  \nlinebreak"
 assert markdownify(html) == text
+
+# bonus 2, test bold
+assert markdownify("There's some <strong>bold text</strong> here!") ==\
+    "There's some **bold text** here!"
